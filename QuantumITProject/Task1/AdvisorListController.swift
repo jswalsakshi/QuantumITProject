3747e59d79fb66d3f74d7a6639686fbf576accd6
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-05-28 00:29:29 +0530

    Set up

---
+++ QuantumITProject/Task1/AdvisorListController.swift
@@ -0,0 +1,17 @@
+//
+//  AdvisorListController.swift
+//  QuantumITProject
+//
+//  Created by Sakshi Jaiswal on 27/05/20.
+//  Copyright © 2020 Sakshi Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+class AdvisorListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
